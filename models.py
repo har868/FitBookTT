833308b285e910f5be9d6ba10fca148b7f9a49e0
har868 <38847693+har868@users.noreply.github.com>
2021-04-25 05:56:15 -0400

    Update models.py

--- models.py
+++ models.py
@@ -7,9 +7,9 @@ db = SQLAlchemy()
 
 class User(UserMixin,db.Model):
 	id = db.Column(db.Integer,primary_key=True)
-	username = db.Column(db.String(80),nullable=False)
-	email = db.Column(db.String(120),nullable=False)
-	password = db.Column(db.String(120),nullable=False)
+	username = db.Column(db.String(80),nullable=False,unique=True)
+	email = db.Column(db.String(120),nullable=False,unique=True)
+	password = db.Column(db.String(120),nullable=False,unique=True)
 	
 	def toDict(self):
 		return{
